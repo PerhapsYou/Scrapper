85de19589d6bf00e99ae5a12d102a5f6756e1dc6
Rithik Tank <rithiktank358@gmail.com>
2025-07-03 16:34:36 +0800

    FIxed bugs across the system

--- RAG/rag_pipeline.py
+++ RAG/rag_pipeline.py
@@ -1,4 +1,4 @@
-from langchain_community.embeddings import HuggingFaceInstructEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_ollama import ChatOllama
 import torch
@@ -10,8 +10,8 @@ llmModel = None  # Initialize llmModel to be used later in the class
 class RAGPipeline:
     def __init__(self, llm_backend: str = "ollama"):
         # Load embeddings and vector store
-        self.embedding = HuggingFaceInstructEmbeddings(
-             model_name="hkunlp/instructor-xl",
+        self.embedding = HuggingFaceEmbeddings(
+             model_name="sentence-transformers/all-MiniLM-L6-v2",
             # If you have a GPU, set device to "cuda", otherwise use "cpu"
              model_kwargs={"device": "cuda" if torch.cuda.is_available() else "cpu"} 
              )
@@ -20,14 +20,15 @@ class RAGPipeline:
             self.embedding,
             allow_dangerous_deserialization=True
         )
-        self.retriever = self.vector_store.as_retriever()
+        self.retriever = self.vector_store.as_retriever(k=5)
 
         # Select LLM backend
         print("✅ Using Ollama (llama3) as LLM")
         try:
             self.llmModel = ChatOllama(
-                model="tuned-model", 
+                model="llama3", 
                 base_url="http://ollama:11434",
+                temperature=0.5,
                 streaming=True  # Enable streaming
                 )
             print("Connecting to Ollama at:", self.llmModel.base_url)
@@ -45,7 +46,23 @@ class RAGPipeline:
 
         context = "\n\n".join(doc.page_content for doc in docs)
         print(context)
-        prompt = f"Use the context below to answer the question.\n\nContext: {context}\n\nQuestion: {question}. Do not say context just give me an answer. If you do not know just apologize and say you do not know.."
+        prompt = f"""Answer the question using only the information provided below.
+                Context:
+                {context}
+
+                Question:
+                {question}
+
+                Instructions:
+                - Answer the question without adding any introductory or concluding phrases. 
+                - Do not repeat or refer to the context.
+                - If the answer cannot be determined from the context, respond with: "I'm sorry, I don't know." 
+                - Be concise and accurate.
+                - In writing your answer make sure there are no dashes.
+                - Return your response as markdown.
+                - Do not output in a single paragraph.
+                - Use dashes instead of asterisks in the markdown.
+                """
 
         for chunk in self.llmModel.stream(prompt):
             yield chunk.content
